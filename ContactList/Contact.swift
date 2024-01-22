7651fafc22eda89f4097f45e3a84e0fd7019d4bf
rahilyanz <rahaomurzakova@gmail.com>
2024-01-22 15:46:51 +0600

    HW 3

---
+++ ContactList/Contact.swift
@@ -0,0 +1,8 @@
+//
+//  Contact.swift
+//  ContactList
+//
+//  Created by Rahilya Nazaralieva on 22/1/24.
+//
+
+import Foundation
---
+++ ContactList/MakerView.swift
@@ -0,0 +1,8 @@
+//
+//  MakerView.swift
+//  ContactList
+//
+//  Created by Rahilya Nazaralieva on 21/1/24.
+//
+
+import Foundation
